355a38551bbd910267de50c68516b6e01ddd9cbc
Ali Zaraket <alizaraket600@gmail.com>
2022-07-04 15:44:53 +0300

    connected the auth status to the app

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,18 @@
 import './Navbar.css'
-import { AiOutlineUserAdd } from 'react-icons/ai'
 import { FiSearch } from 'react-icons/fi'
 import Modal from './Modal'
 import { useState } from 'react'
 import { useUsersData } from '../../context/UsersContext'
 import { useMessage } from '../../context/MessageContext'
+import DropDown from '../ui/Dropdown'
+import { AiOutlineUserAdd } from 'react-icons/ai'
+import { BsDot } from 'react-icons/bs'
 
 export default function Navbar() {
   const [newUserModal, setNewUserModal] = useState(false)
   const [newUserName, setNewUserName] = useState("")
 
-  const { addUser, searchUsers } = useUsersData()
+  const { addUser, searchUsers, auth, setAuthOn, setAuthOff } = useUsersData()
   const { showMessage } = useMessage()
 
   function closeModal() {
@@ -62,10 +64,19 @@ export default function Navbar() {
           <input className='pill-shape-input' type="text" placeholder="Search..." onChange={searchUsers} />
         </div>
       </div>
-      <button className="new-user" onClick={() => setNewUserModal(true)}>
-        <span><AiOutlineUserAdd size={20} /></span>
-        New user
-      </button>
+      <DropDown>
+        <div onClick={() => setNewUserModal(true)}>
+          <p>New user</p>
+          <AiOutlineUserAdd size={24} />
+        </div>
+        <div onClick={auth ? setAuthOff : setAuthOn}>
+          <p>Auth state</p>
+          <BsDot 
+            size={24}
+            color={auth ? "rgb(var(--clr-green))" : "rgb(var(--clr-red))"}
+          />
+        </div>
+      </DropDown>
     </div>
   )
 }
